3cd4339417d676b4304b8aeca80c9224ac365435
Jacob Biehler <biehlerj@gmail.com>
2018-05-01 15:05:01 -0700

    Optimizing FizzBuzz

--- learn_how_to_code-golang/exercises/fizzbuzz/fizzbuzz.go
+++ learn_how_to_code-golang/exercises/fizzbuzz/fizzbuzz.go
@@ -4,18 +4,22 @@ package main
 import "fmt"
 
 func main() {
-	fizzString := "Fizz"
-	buzzString := "Buzz"
 	for i := 1; i < 101; i++ {
-		if i%15 == 0 {
-			fmt.Print(fizzString + buzzString)
-		} else if i%3 == 0 {
-			fmt.Print(fizzString)
-		} else if i%5 == 0 {
-			fmt.Print(buzzString)
-		} else {
-			fmt.Printf("%d", i)
+		result := ""
+		if i%3 == 0 {
+			result += "Fizz"
 		}
+		if i%5 == 0 {
+			result += "Buzz"
+		}
+		if result != "" {
+			fmt.Print(result)
+			if i != 100 {
+				fmt.Print(" ")
+			}
+			continue
+		}
+		fmt.Print(i)
 		if i != 100 {
 			fmt.Print(" ")
 		}
